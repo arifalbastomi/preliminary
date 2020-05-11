e9b2ec081771a2fa12dbd42aa2f023b40e77eb6f
arif.albastomi <tomyysantoso23@gmail.com>
2020-05-11 14:46:25 +0700

    number1

--- Algorithm/number1/main.go
+++ Algorithm/number1/main.go
@@ -1,1 +1,26 @@
-package number1
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main(){
+
+	var n int = 9
+	var numbers = []int{10,20,20,10,10,30,50,10,20}
+	sort.Ints(numbers)
+
+	var count int =0
+
+	for i := 0 ; i< n-1 ; i++{
+		if(i < n && numbers[i] == numbers[i+1]){
+			count++
+			i = i + 1
+		}
+
+	}
+
+	fmt.Printf("total %d \n", count)
+
+}
