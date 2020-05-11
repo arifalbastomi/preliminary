fb56e4046d54903415d5db334df2d37141640b48
arif.albastomi <tomyysantoso23@gmail.com>
2020-05-11 17:02:23 +0700

    number2

--- Algorithm/number2/main.go
+++ Algorithm/number2/main.go
@@ -1,1 +1,36 @@
-package number2
+package main
+
+import("fmt")
+
+func main(){
+
+	var n int = 8
+	var steps  = []string{"U","D","D","D","U","D","U","U"}
+
+	var down bool = false
+	var position int = 0
+	var valleys int =0
+
+	for i:=0 ; i<n ; i++ {
+
+		if(steps[i]=="U"){
+			position++
+		}else{
+			position--
+		}
+
+		if(!down && position<0){
+			valleys++
+			down=true
+		}
+
+		if(position>=0){
+			down=false
+		}
+
+	}
+
+	fmt.Printf("valley:  %d \n", valleys)
+
+}
+
