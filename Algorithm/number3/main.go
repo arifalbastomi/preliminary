6bb8c651ac70d171012645cce55320266d9597ac
arif.albastomi <tomyysantoso23@gmail.com>
2020-05-11 13:44:23 +0700

    number3

--- Algorithm/number3/main.go
+++ Algorithm/number3/main.go
@@ -1,1 +1,31 @@
-package number3
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func main(){
+
+	var input int = 1345679
+    var conv  = strconv.Itoa(input)
+	var len = len(conv)
+	newArr:= strings.Split(conv, "")
+	var counter2  =0
+	for len >= 0 {
+		var counter=0
+		for j := 1; j <=len; j++ {
+			if (counter==0){
+				fmt.Print(newArr[counter2])
+			}else{
+				fmt.Print(0)
+			}
+			counter++
+		}
+		counter2++
+		fmt.Println();
+		len--
+	}
+
+}
