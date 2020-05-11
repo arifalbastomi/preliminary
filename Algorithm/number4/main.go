17aa3e71bae8e48ec29b71f2b2043050910a1582
arif.albastomi <tomyysantoso23@gmail.com>
2020-05-11 17:40:35 +0700

    number4

--- Algorithm/number4/main.go
+++ Algorithm/number4/main.go
@@ -1,1 +1,33 @@
-package number4
+package main
+
+import "fmt"
+
+func main(){
+
+	var n int  = 100
+	var lamp int= 1
+	var next int = 1
+	var on int= 0
+
+	for lamp = 1; lamp <= n; lamp++ {
+
+		var counter int= 0
+		for next = 1; next * next <= n; next++ {
+
+			if (lamp % next == 0) {
+				counter++
+
+				if (lamp / next != next){
+					counter++
+				}
+			}
+		}
+
+		if (counter % 2 == 1){
+			on++
+		}
+	}
+	fmt.Printf("the number of lights that are on: %d  \n", on)
+}
+
+
